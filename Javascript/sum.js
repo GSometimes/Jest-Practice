69b2e09562be487ce6473ffdfb4a2984fd26a716
Gourav <gore.auluck@gmail.com>
2021-03-06 23:44:35 -0800

    Jest Practice

--- Javascript/sum.js
+++ Javascript/sum.js
@@ -1,9 +1,12 @@
+// Jest is a JavaScript framework that allows you to write tests to check the effectiveness of your code.
+// In this example from the documentation I will be creating a simple function
+// and then testing said function with a .test.js file.
+
+
+// Create/Export 'sum' function that adds two numbers together.
+
 function sum (a, b) {
     return a + b
 }
-
-// function helper() {
-//     console.log('Helper')
-// }
 
 module.exports = sum
--- Javascript/sum.test.js
+++ Javascript/sum.test.js
@@ -1,6 +1,12 @@
+// The .test.js file is where you'll be testing your code to ensure
+// that it was written and runs properly.
+
+// Import 'sum function
+
 const { expect } = require('@jest/globals')
 const sum = require('./sum')
 
-test('properly adds two numbers', () => {
-    expect(sum(1, 2)).toBe(3)
+test('properly adds two numbers', () =>{
+    expect(sum(1, 2)).toBe(3) // In a .test.js file 'expect' is the equivalent of an 'if' statement.
 })
+
