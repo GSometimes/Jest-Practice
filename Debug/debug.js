2adffa82d613283fd7a9b167c620814ff0ca4a24
Gourav <gore.auluck@gmail.com>
2021-03-07 16:23:46 -0800

    AM Debug Masterclass

--- Debug/debug.js
+++ Debug/debug.js
@@ -12,36 +12,58 @@
 
 // console.log(cheese)
 
-const reverseString = (str) => {
-    // return str.split().reverse().join() 
-    // Break into smaller steps and console.log each step.
+// const reverseString = (str) => {
+//     // return str.split().reverse().join() 
+//     // Break into smaller steps and console.log each step.
 
-    // Console.count helps you count steps along the way to see where there may be an error in your code.
-    // Shows how many times method has been called.
-    // console.count("reverseString")
+//     // Console.count helps you count steps along the way to see where there may be an error in your code.
+//     // Shows how many times method has been called.
+//     // console.count("reverseString")
 
-    // See how long a function is taking to run.
-    console.time("reverseString") // Starts timer
+//     // See how long a function is taking to run.
+//     console.time("reverseString") // Starts timer
 
-    const split = str.split("") // .split()
-    // console.log(split)
-    // console.count("reverseString")
+//     const split = str.split("") // .split()
+//     // console.log(split)
+//     // console.count("reverseString")
 
     
-    const reverse = split.reverse() // .reverse()
-    // console.log(reverse)
-    // console.count("reverseString")
+//     const reverse = split.reverse() // .reverse()
+//     // console.log(reverse)
+//     // console.count("reverseString")
 
 
-    const final = reverse.join("") // .join()
-    // console.log(final)
-    // console.count("reverseString")
+//     const final = reverse.join("") // .join()
+//     // console.log(final)
+//     // console.count("reverseString")
 
-    console.timeEnd("reverseString") //Ends timer
-    return final
+//     console.timeEnd("reverseString") //Ends timer
+//     return final
+// }
+
+
+const reverseString = (str) => {
+    // console.log(typeof str)
+    console.log(str instanceof Array)
+    if(typeof str === "string"){
+        return str.split("").reverse().join("")
+    } else {
+        // console.error("this is not a string")
+        throw("This is not a string")
+    }
+};
+
+//try catch block
+try {
+    console.log(reverseString([1, 1, 1, 1, 1]));
+}
+catch(error){
+    console.log(error)
 }
 
-console.log(reverseString("Hello"))
+// console.log(reverseString("Hello"));
+// console.log(reverseString(1));
+// console.log(reverseString([1, 1, 1, 1, 1]));
 
 // console.table({name: "Gore Sometimes", age: 32})
 // A technique to check data within an object.
